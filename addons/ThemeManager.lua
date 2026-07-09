5d3c69d5ebbd04fbf7c9bb0203b923b1cd0ea2ba
LolStudioLibs <fabioarapi02@gmail.com>
2026-07-09 20:41:22 +0300

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'UnnamedLibSettings'
+	ThemeManager.Folder = 'unnamedLibSettings'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
